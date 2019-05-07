cd1508dd7ee8448e2c34c627be4088f8e853b301
Britta Jorgenson <bljorgen@gmail.com>
2019-05-07 00:56:52 +0000

    changed index.js to use .on('finish'... vs..on('close'....

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
             console.log('Downloading image...')
         })
         .pipe(fs.createWriteStream('./future.jpg')
-        .on('close', () => {
+        .on('finish', () => {
             console.log('Download Complete')
         }));
