5f05da13da9ff93faa129fbfe61d39b633d00006
Britta Jorgenson <bljorgen@gmail.com>
2019-05-07 00:20:51 +0000

    Added index.js and successfully downloaded future.jpg.

---
+++ index.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+        .on('error', err => { throw err; })
+        .on('response', response => {
+            console.log(`
+            Response code : ${response.statusCode}
+            Response message: ${response.statusMessage}
+            Content Type: ${response.headers['content-type']}
+            `);
+            console.log('Downloading image...')
+        })
+        .pipe(fs.createWriteStream('./future.jpg')
+        .on('close', () => {
+            console.log('Download Complete')
+        }));
